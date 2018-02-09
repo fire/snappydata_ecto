0dc113afb0545d1794f1fd3466b4b4bcd9794cec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-02-09 14:59:11 -0800

    Add a test bash script and update dependencies.

---
+++ test.sh
@@ -0,0 +1,6 @@
+export MIX_ENV=snappydata
+export SNAPPYDATA_PORT=31320
+export SNAPPYDATA_HOST="192.168.0.23"
+export TZ=Etc/UTC
+#mix test deps/snappyex/test/login_test.exs:16 || true
+mix test --trace
