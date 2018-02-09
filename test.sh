d8b3eca2786a48754c24b08d61a9de82277ff5fd
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-02-09 15:29:05 -0800

    Don't display the correct entries.

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ export SNAPPYDATA_PORT=31320
 export SNAPPYDATA_HOST="192.168.0.23"
 export TZ=Etc/UTC
 #mix test deps/snappyex/test/login_test.exs:16 || true
-mix test --trace --seed 0
+mix test --seed 0 # --trace
